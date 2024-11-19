026ad240ce0cdcacb18a94e32cf82924024e5dcc
Komiljonovdev <uzokdeveloper@gmail.com>
2024-11-19 20:11:59 +0500

    MINOR: Implemented guzzlehttp

--- src/Bot.php
+++ src/Bot.php
@@ -1,14 +1,25 @@
 <?php
+require "vendor/autoload.php";
+use GuzzleHttp\Client;
 class Bot {
     const API_URL = 'https://api.telegram.org/bot';
     private $token = '7500571910:AAHkgCg7PsTlH91ltgQlaDzBWT6-tljwtmY';
+    public $client;
     public function makeRequest($method, $data = []) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, self::API_URL . $this->token . '/' . $method);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return json_decode($response);
+        $this->client = new Client([
+            'base_uri' => self::API_URL . $this->token,
+            'timeout'  => 2.0,
+        ]);
+
+        $request = $this->client->request('POST', '/' . $method, ['json' => $data]);
+
+        return json_decode($request->getBody()->getContents());
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, self::API_URL . $this->token . '/' . $method);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//        $response = curl_exec($ch);
+//        curl_close($ch);
+//        return json_decode($response);
     }
 }
